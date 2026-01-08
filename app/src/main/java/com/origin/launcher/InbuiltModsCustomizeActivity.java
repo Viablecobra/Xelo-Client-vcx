be0b8a91d3b7b6feb11b06ecbaa4f2ffd0763487
vcx <ryansk0987@gmail.com>
2026-01-08 15:38:29 +0530

    update InbuiltModsCustomizeActivity.java

--- app/src/main/java/com/origin/launcher/InbuiltModsCustomizeActivity.java
+++ app/src/main/java/com/origin/launcher/InbuiltModsCustomizeActivity.java
@@ -224,7 +224,7 @@ adapterContainer.layout(0, 0, adapterContainer.getMeasuredWidth(), adapterContai
                     adapterContainer.setTranslationX(panelW);
                     adapterContainer.animate().translationX(0f).setDuration(duration).start();
 
-                                        float slide = panelW - dpToPx(30);
+                                        float slide = panelW - dpToPx(50);
 bottomButtons.animate().translationX(-slide).setDuration(duration).start();
                     bottomButtons.animate().translationX(-slide).setDuration(duration).start();
                 } else {
